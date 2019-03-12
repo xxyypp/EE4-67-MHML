2f06292edf15584942c57c3ad0b71fc421fee096
xxyypp <yangpuypyp@outlook.com>
2019-03-12 15:15:42 +0000

    regular update

--- ml/code/socket.py
+++ ml/code/socket.py
@@ -5,7 +5,8 @@ import decision_tree
 '''Edit ip address before use'''
 #ip = 'http://35.246.29.217:65080/' 
 #ip = 'http://146.169.173.109:65080/' 
-ip = 'http://192.168.0.27:65080'
+#ip = 'http://192.168.0.27:65080'
+ip = 'http://localhost:65080'
 
 sio = socketio.Client()
 
@@ -15,22 +16,23 @@ def on_connect():
 
 
 '''change topic name before use'''
-@sio.on('new message')
+@sio.on('newTime')
 def on_message(data):
-    # might need to change the first line base on different
-    # enivronment, e.g. might not have 'message' before msg
-    data = json.loads(data['message'])
-
-    # Decode json:
+    print(data)
+    #data = json.loads(data['message'])
     hour = data['hour']
     minute = data['minute']
     second = data['second']
     # call function to increase level
+    #print(hour)
+    #print(minute)
+    #print(second)
     t = DecisionTree(hour,minute)
     level = t.analysis()
-    print('predicted level: ', level)
-    
-    #sio.emit('topic', level)
+    #print('predicted level: ', level.tolist())
+    #print(level[0]-1)
+    #print(type(level[0]-1))
+    sio.emit('newAlertLevel', level[0])
 
 
 @sio.on('disconnect')
